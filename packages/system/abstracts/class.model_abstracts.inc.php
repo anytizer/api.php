3977f5f9447a06bf39d336d4a334258f98e437f0
anytizer <anytizer@bimal.org.np>
2019-09-21 11:18:26 -0600

    PDO connection options

--- packages/system/abstracts/class.model_abstracts.inc.php
+++ packages/system/abstracts/class.model_abstracts.inc.php
@@ -17,7 +17,14 @@ abstract class model_abstracts
              * Connects to the database server
              * This is the only configuration for your database, through out the entire application
              */
-            $this->pdo = new PDO("mysql:host=localhost;dbname=inventory", "root", "");
+            $options = array(
+                PDO::ATTR_PERSISTENT => false,
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4;",
+                PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => false,
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            );
+            $this->pdo = new PDO("mysql:host=localhost;dbname=inventory", "root", "", $options);
         } catch (Exception $ex) {
             die("Cannot connect to the database. See: " . basename(__FILE__));
         }
