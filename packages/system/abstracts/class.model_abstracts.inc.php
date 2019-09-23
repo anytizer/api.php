5bb3be1483b0fa363e21802c4a0444fd728c23ca
anytizer <anytizer@bimal.org.np>
2019-09-22 22:58:00 -0600

    additional model functions.

--- packages/system/abstracts/class.model_abstracts.inc.php
+++ packages/system/abstracts/class.model_abstracts.inc.php
@@ -17,10 +17,61 @@ abstract class model_abstracts
              * Connects to the database server
              * This is the only configuration for your database, through out the entire application
              */
-            $this->pdo = new PDO("mysql:host=localhost;dbname=inventory", "root", "");
+            $options = array(
+                PDO::ATTR_PERSISTENT => false,
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4;",
+                PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => false,
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            );
+            // @todo Read configs from outside public_html
+            $this->pdo = new PDO("mysql:host=192.168.1.76;dbname=passport", "admin", "nimda", $options);
         } catch (Exception $ex) {
             die("Cannot connect to the database. See: " . basename(__FILE__));
         }
     }
 
+    /**
+     * Queries that should draw one row or the details.
+     *
+     * @param string $sql
+     * @param array $parameters
+     * @return array
+     */
+    protected function single($sql = "", $parameters = []): array
+    {
+        $statement = $this->pdo->prepare($sql);
+        $success = $statement->execute($parameters);
+        $result = $statement->fetch(PDO::FETCH_ASSOC);
+        return $result;
+    }
+
+    /**
+     * Queries that should return a list of rows.
+     *
+     * @param string $sql
+     * @param array $parameters
+     * @return array
+     */
+    protected function rows($sql = "", $parameters = []): array
+    {
+        $statement = $this->pdo->prepare($sql, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
+        $success = $statement->execute($parameters);
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
+    }
+
+    /**
+     * Queries that should return boolean. Eg. flag something, or insert/update a record.
+     *
+     * @param string $sql
+     * @param array $parameters
+     * @return bool
+     */
+    protected function query($sql = "", $parameters = []): bool
+    {
+        $statement = $this->pdo->prepare($sql);
+        $success = $statement->execute($parameters);
+        return $success;
+    }
 }
